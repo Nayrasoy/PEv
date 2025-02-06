ebc31567f193fb2a108c7c4fa8eb40e9a781e8be
Carlos Vivero Barrera <carlos.3vb@gmail.com>
2025-02-06 21:05:23 +0100

    Mi primer commit :)

--- Practica1/src/MainP1.java
+++ Practica1/src/MainP1.java
@@ -1,4 +1,4 @@
-public class Main {
+public class MainP1 {
 
     public static void main(String args[]) {
         System.out.println("Hola mundo!");
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PEv
+Práctica 1 de Programación Evolutiva:
+ - Comenzada el 7/2/2025
