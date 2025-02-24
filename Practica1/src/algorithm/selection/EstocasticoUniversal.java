ddce96f1314ecac70fdc2c313d600481708c17eb
Carlos Vivero Barrera <carlos.3vb@gmail.com>
2025-02-25 00:04:16 +0100

    EstocasticoUniversal hecho

--- Practica1/src/algorithm/selection/EstocasticoUniversal.java
+++ Practica1/src/algorithm/selection/EstocasticoUniversal.java
@@ -1,15 +1,50 @@
 package algorithm.selection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Individuo;
+import utils.Utils;
 
 public class EstocasticoUniversal extends SelectionMethod {
 
     @Override
     public List<Individuo> selection(List<Individuo> poblation, List<Double> fitness, double fitnessSum, int n) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'selection'");
+        List<Double> probabilities = new ArrayList<>();
+        List<Individuo> newPoblation = new ArrayList<>(); 
+        double actual = 0, probability, random, sumValue = 1 / n;
+        double minValue = poblation.get(0).getMinValue();
+
+        if (minValue < 0) {
+            fitnessSum += minValue * poblation.size();
+        }
+    
+        for (int i = 0; i < fitness.size(); i++) {
+            probability = fitness.get(i);
+            if (minValue < 0) {
+                probability += minValue;
+            }
+            probability /= fitnessSum;
+            actual += probability;
+            probabilities.add(actual);
+        }
+
+        random = Utils.random.nextDouble();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < probabilities.size(); j++) {
+                Double p = probabilities.get(j);
+                if (random < p) {
+                    newPoblation.add(poblation.get(j));
+                    break;
+                }
+            }
+            random += sumValue;
+            if (random > fitnessSum) {
+                random -= fitnessSum;
+            }
+        }
+
+        return newPoblation;
     }
 
     @Override
