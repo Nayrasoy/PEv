048337a7a13bd9322f83df00b6379685ebb89f1a
Nayrasoy <naybetan@ucm.es>
2025-02-24 23:33:50 +0100

    Metodo restos

--- Practica1/src/algorithm/selection/Restos.java
+++ Practica1/src/algorithm/selection/Restos.java
@@ -1,8 +1,10 @@
 package algorithm.selection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Individuo;
+import utils.Utils;
 
 public class Restos extends SelectionMethod {
 
@@ -10,8 +12,37 @@ public class Restos extends SelectionMethod {
 
     @Override
     public List<Individuo> selection(List<Individuo> poblation, List<Double> fitness, double fitnessSum) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'selection'");
+        List<Individuo> newPoblation = new ArrayList<>(); 
+        int k = poblation.size();
+        double probability, random;
+        double minValue = poblation.get(0).getMinValue();
+
+        if (minValue < 0) {
+            fitnessSum += minValue * poblation.size();
+        }
+    
+        for (int i = 0; i < fitness.size(); i++) {
+            probability = fitness.get(i);
+            if (minValue < 0) {
+                probability += minValue;
+            }
+            probability /= fitnessSum;
+            if (Math.floor(probability*k)){
+
+            }
+            else {
+                lista ordenada
+            }
+            for (int j = 0; j < ; j++){
+                newPoblation.add(poblation.get(i));
+            }
+        }
+
+        for (int i = newPoblation.size(); i < k, i++){
+            seleccionamos
+        }
+
+        return newPoblation;
     }
 
     @Override
