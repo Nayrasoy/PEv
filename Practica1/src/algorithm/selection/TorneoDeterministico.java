27842d8e1d00107a95e56729500e37e3b8330763
Nayrasoy <naybetan@ucm.es>
2025-02-25 16:33:44 +0100

    Torneos hecho

--- Practica1/src/algorithm/selection/TorneoDeterministico.java
+++ Practica1/src/algorithm/selection/TorneoDeterministico.java
@@ -1,17 +1,37 @@
 package algorithm.selection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import config.Parameters;
 import model.Individuo;
+import utils.Utils;
 
 public class TorneoDeterministico extends SelectionMethod {
 
     @Override
     public List<Individuo> selection(List<Individuo> poblation, List<Double> fitness, double fitnessSum, int n) {
         List<Individuo> newPoblation = new ArrayList<>();
+        int k = Parameters.TOURNAMENT_K;
         
-        
+        while (newPoblation.size() < n) {
+            List<Individuo> tournament = new ArrayList<>();
+
+            for (int i = 0; i < k; i++) {
+                int random = Utils.random.nextInt(n);
+                tournament.add(poblation.get(random));
+            }
+
+            int bestIndex = 0;
+            for (int i = 1; i < k; i++) {
+                if (tournament.get(i).getFitness() > tournament.get(bestIndex).getFitness()) {
+                    bestIndex = i;
+                }
+            }
+
+            newPoblation.add(tournament.get(bestIndex));
+        }
 
         return newPoblation;
     }
--- Practica1/src/config/Parameters.java
+++ Practica1/src/config/Parameters.java
@@ -18,4 +18,8 @@ public class Parameters {
     public static int DEFAULT_TAM_TOURNAMENT = 3;
     public static int DEFAULT_DIMENSION = 5;
 
+    public static double PROPORSION_TRUNCAMIENTO = 0.5;
+    public static int TOURNAMENT_K = 3;
+    public static double TOURNAMENT_P = 0.5;
+
 }
